14c6e7146ae33a4892fa4f1a8f8023b3347d04d6
Dan6erbond <moravrav@gmail.com>
2019-04-19 00:24:32 +0200

    initial commit

--- login.py
+++
@@ -1,6 +0,0 @@
-import praw
-
-def get_praw():
-    return praw.Reddit(user_agent='Dan6erbond',
-    client_id='GgijdvupxYwS_Q', client_secret="csrlC4DwCV77b_EXxTQK064XsrY",
-    username='Dan6erbond', password='Ravi!MoaR')
